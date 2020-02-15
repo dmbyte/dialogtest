ddfd60713de8ddbd9f0fdd6ff93a84b31431f9af
Martin Kealey <from-github@kurahaupo.gen.nz>
2020-02-15 17:54:52 +1100

    Update buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -3,7 +3,6 @@
 BACKTITLE="David's SUSE Enterprise Storage Installer"
 MYHOME=~
 DIALOGTMP=/tmp/buildit.sh.$$
-TMPLOG_FILE=/tmp/buildit.log.$$
 
 editfile(){
         FILENAME=$1
@@ -32,30 +31,15 @@ passwdbox(){
         rm $DIALOGTMP
 
 }
-outputtolog(){
-touch $TMPLOG_FILE
 
-# Close STDOUT file descriptor
-exec 1<&-
-# Close STDERR FD
-exec 2<&-
+exec 3>&1 4>&2
 
-# Open STDOUT as $TMPLOG_FILE file for read and write.
-exec 1<>$TMPLOG_FILE
-
-# Redirect STDERR to STDOUT
-exec 2>&1
-}
-
-outputtoscreen(){
-exec 1>&0
-exec 2>&0
-#rm -f $TMPLOG_FILE
-}
-touch $TMPLOG_FILE
-dialog --backtitle "$BACKTITLE" --tailboxbg "$TMPLOG_FILE" 40 40
-outputtolog
-#dialog --backtitle "$BACKTITLE" --tailbox "/var/log/messages" 40 40
-echo "this should be in the log"
-outputtoscreen
-echo "this should be on the screen"
+dialog --backtitle "$BACKTITLE" --tailboxbg <(
+                exec 2>&1
+                echo "this should be in the log"
+                echo "this should be on the screen" >&3
+                for (( i=0; i<1000 ; ++i )) do
+                        sleep 5
+                        echo $i
+                done
+        ) 40 40
